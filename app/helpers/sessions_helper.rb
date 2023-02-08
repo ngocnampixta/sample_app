ac1317baf1dbc7b47b737cc2e54658654321d05f
ngocnam <ngocnamk3er@gmail.com>
2023-02-09 00:14:07 +0700

    use only cookie to login and logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,21 @@
 module SessionsHelper
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+  # def log_in(user)
+  #   session[:user_id] = user.id
+  # end
 
   def current_user
-    if session[:user_id]
-      User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id]) # knowsonly about the temporary session # not is a comparison
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      puts "-------------------"
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        # log_in user
+        puts "-------------------login user"
+        # cookies.permanent.encrypted[:user_id] = user.id
+        # cookies.permanent[:remember_token] = user.remember_token
+        @current_user = user
+      end
     end
   end
 
@@ -14,7 +24,13 @@ module SessionsHelper
   end
 
   def log_out
-    session.delete(:user_id)
-    @current_user = nil
+    cookies.permanent.encrypted[:user_id] = nil
+    cookies.permanent[:remember_token] = nil
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 end
